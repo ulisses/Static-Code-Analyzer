87a7c03bc478c6fde830c9814b758eeb05bb7094
cartas <cartas@cartas-pc.(none)>
2010-11-04 22:18:27 +0000

    experiencia a adicionar um projecto netbeans

---
+++ projecto_exp/src/projecto_exp/exp.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package projecto_exp;
+
+/**
+ *
+ * @author cartas
+ */
+public class exp {
+
+    public exp(){}
+}
