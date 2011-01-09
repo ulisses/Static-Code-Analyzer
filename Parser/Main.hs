4fe0db03122d6fe2c74165d71f08dcd40427485a
Ulisses Costa <ulissesmonhecosta@gmail.com>
2011-01-09 06:40:36 +0000

    submit C.Language Haskell parser (we still need to extract the comments)

---
+++ Parser/Main.hs
@@ -0,0 +1,18 @@
+module Main where
+
+import Language.C
+import Language.C.System.GCC
+import System.Environment
+
+process :: String -> IO ()
+process file = do
+	putStr file
+	stream <- readInputStream file
+	putStr (take (20 - length file) $ repeat ' ')
+	either print (const $ putStrLn "Pass") (parseC stream nopos)
+
+main :: IO ()
+main = do
+	files <- getArgs
+	mapM_ process files
+
