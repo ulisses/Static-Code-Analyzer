1f0de7fe973b0a313499d79734beb8b7b8b0f779
Ulisses Costa <ulissesmonhecosta@gmail.com>
2011-04-03 02:02:34 +0100

    graficos para participantes do consurso... vou dormir

--- Haskell/graphForContest/Main.hs
+++ Haskell/graphForContest/Main.hs
@@ -1,4 +1,5 @@
-module Test14a where  
+{-# OPTIONS -XNoMonomorphismRestriction #-}
+module Main where  
 
 import Graphics.Rendering.Chart
 import Graphics.Rendering.Chart.Gtk
@@ -14,30 +15,50 @@ import Prices(prices1)
 
 chart :: Double -> Renderable ()
 chart lwidth = toRenderable layout
-	where
-		layout = layout1_title ^="Price History"
-			$ layout1_background ^= solidFillStyle (opaque white)
-			$ layout1_left_axis ^: laxis_override ^= axisTicksHide
-			$ layout1_plots ^= [ Left (toPlot price1), Left (toPlot spots) ]
-			$ setLayout1Foreground (opaque black)
-			$ defaultLayout1
-		price1 = area_spots_4d_title ^= "random value"
-			$ area_spots_4d_max_radius ^= 20
-			$ area_spots_4d_values ^= values
-			$ defaultAreaSpots4D
+    where
+        layout = layout1_title ^="Price History"
+            $ layout1_background ^= solidFillStyle (opaque white)
+            $ layout1_left_axis ^: laxis_override ^= axisTicksHide
+            $ layout1_plots ^= [ Left (toPlot price1), Left (toPlot spots) ]
+            $ setLayout1Foreground (opaque black)
+            $ defaultLayout1
+        price1 = area_spots_4d_title ^= "random value"
+            $ area_spots_4d_max_radius ^= 20
+            $ area_spots_4d_values ^= values
+            $ defaultAreaSpots4D
+        spots = area_spots_4d_title ^= "random value"
+            $ area_spots_4d_max_radius ^= 20
+            $ area_spots_4d_values ^= values
+            $ defaultAreaSpots4D
 
-		points = map (\ (d,v,z,t)-> (d,v) ) values
-		values = [ (d, v, z, t) | ((d,v,_),z,t) <- zip3 prices1 zs ts ]
-		zs,ts :: [Int]
-		zs     = randoms $ mkStdGen 0
-		ts     = randomRs (-2,27) $ mkStdGen 1
-		lineStyle = line_width ^= 3 * lwidth
-			$ line_color ^= opaque blue
-			$ defaultPlotLines ^. plot_lines_style
+        points = map (\ (d,v,z,t)-> (d,v) ) values
+        values = [ (d, v, z, t) | ((d,v,_),z,t) <- zip3 prices1 zs ts ]
+        zs,ts :: [Int]
+        zs     = randoms $ mkStdGen 0
+        ts     = randomRs (-2,27) $ mkStdGen 1
+        lineStyle = line_width ^= 3 * lwidth
+            $ line_color ^= opaque blue
+            $ defaultPlotLines ^. plot_lines_style
 
 main1 :: [String] -> IO (PickFn ())
 main1 ["small"]  = renderableToPNGFile (chart 0.25) 320 240 "test14_small.png"
 main1 ["big"]    = renderableToPNGFile (chart 0.25) 800 600 "test14_big.png"
 main1 _          = renderableToWindow  (chart 1.00) 640 480 >> return undefined
 
-main = getArgs >>= main1
+main = getArgs >>= main2
+
+main2 _ = renderableToPNGFile (toRenderable l) 320 320 "out.png"
+l = layout1_title ^="Price History"
+    $ layout1_plots ^= [ Left (toPlot f) ]
+    $ defaultLayout1
+
+
+f = area_spots_4d_title ^= "random value"
+    $ area_spots_4d_max_radius ^= 20
+    $ area_spots_4d_values  ^= v
+    $ defaultAreaSpots4D
+	where
+		v :: [(Int,Int,Int,Int)]
+		v =  [(1,2,3,0),(2,3,4,0)]
+
+
