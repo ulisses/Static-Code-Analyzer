6f81dd8a04592adbec3901ef4bc81cdfa4f48592
Ulisses Costa <ulissesmonhecosta@gmail.com>
2011-04-17 19:37:46 +0100

    we have a simple implementation of mcCabe index :)

--- Parser/Main.hs
+++ Parser/Main.hs
@@ -17,129 +17,47 @@ import Data.Generics.Strafunski.StrategyLib.StrategyPrelude
 import Data.Generics.Strafunski.StrategyLib.FlowTheme
 import Control.Monad
 
-teste =  parseCFile (newGCC "gcc") Nothing ["-U__BLOCKS__"] "main.c" >>= return . mcCabeIndex . (\(Right p) -> p)
+parr = parseCFile (newGCC "gcc") Nothing ["-U__BLOCKS__"] "main.c"
 
---mcCabeIndex :: Data a => a -> Int
-mcCabeIndex =  unsafePerformIO . applyTU (full_tdTU step)
+
+{- mcCabe index, we need to have Int as Monoid, in the future
+   we must change this to Sum, because we may want to have
+   (*) as a Monoid too.
+   Read more here: http://blog.sigfpe.com/2009/01/haskell-monoids-and-their-uses.html
+-}
+testMcCabe :: IO Int
+testMcCabe =  parseCFile (newGCC "gcc") Nothing ["-U__BLOCKS__"] "main.c"
+                   >>= (\(Right prog) -> ( mcCabeIndex prog))
 
 instance Num a => Monoid a where
 	mappend = (+)
 	mempty = 0
 
-step = ifTU isConditional (const $ constTU 1) (constTU 0)
+mcCabeIndex :: Data a => a -> IO Int
+mcCabeIndex =  applyTU (full_tdTU isConditional)
 
 isConditional = constTU 0
-                       `adhocTU`  fa
+                          `adhocTU` test
 
---fa :: Monad m => CStat -> m ()
-fa (CIf _ _ _ _) = putStrLn "encontrei um if" >> return 1
-fa a = putStrLn ("encontrei outra cena: " ++ show a) >> return 0
+test (CIf _ _ _ _) = return 1
+test (CSwitch _ _ _) = return 1
+test _ = return 0
 
+{- We may need to import some libraries to be able to put the input code
+   to work, so we must say it to GCC like this:
+   stream <- parseCFile (newGCC "gcc") Nothing ["-Idir"] file
+   where 'dir' is the directory where our code is.
+   This flag is passed as it is directly to GCC, so is just an common import.
+-}
 process :: String -> IO ()
 process file = do
-	--stream <- parseCFile (newGCC "gcc") Nothing ["-I../../../../../2-matricula/1.2/pp2/ex/media/win_c2/code/pp2/2-matricula/pp2_tp3/"] file
+	{- This version of Language.C does not support BLOCKS notation from MacOSX,
+	   so we need to undefine them... Is a fast solution to get our code being parsed
+	-}
 	stream <- parseCFile (newGCC "gcc") Nothing ["-U__BLOCKS__"] file
 	case stream of
 		( Left error  ) -> print error
 		--( Right cprog ) -> f cprog
-		--( Right cprog ) -> print cprog
-
-f :: CTranslUnit -> IO()
-f (CTranslUnit l n) = mapM_ g l
-
-g :: CExtDecl -> IO()
---g (CDeclExt cdecl)   = 
-g (CFDefExt fx) = h fx
---g (CAsmExt CStrLit)  = 
-
-h :: CFunDef -> IO()
-h (CFunDef lDeclSpec cDeclr lDecl cs n) = do
-	mapM_ i lDeclSpec
-	ii cDeclr
-	mapM_ ll lDecl
-	statement cs
-
-statement :: CStat -> IO()
-statement (CReturn (Just cExpr) ni) = do
-	putStr "return "
-	expr cExpr
-statement (CReturn Nothing ni)          = putStrLn "RETURNS"
-statement (CLabel ident cStat lCAttr _) = putStrLn "LABEL"
-statement (CCompound lIdent lCBlockItem _) = do
-	mapM_ (putStrLn . identToString) lIdent
-	mapM_ block lCBlockItem
-
-block :: CBlockItem -> IO()
-block (CBlockStmt cStat) = statement cStat
-
-ii :: CDeclr -> IO()
-ii (CDeclr Nothing lCDerivedDeclr (Just cStrLit) lcAtrb _) = putStrLn "N J"
-ii (CDeclr (Just ident) lCDerivedDeclr Nothing lcAtrb _)   = do
-	
-	let funName = identToString ident
-	putStr funName
-	mapM_ jj lCDerivedDeclr
-	mapM_ kk lcAtrb
-
-kk :: CAttr -> IO()
-kk (CAttr ident lCExpr n) = putStrLn $ identToString ident 
-
-jj :: CDerivedDeclr -> IO()
---jj (CPtrDeclr lCTypeQual nodeInfo)                    = 
---jj (CArrDeclr lCTypeQual cArrSize nodeInfo)           = 
-jj (CFunDeclr (Right (lCDecl, bool)) lCAttr nodeInfo) = do
-	mapM_ ll lCDecl
-	mapM_ kk lCAttr
-jj (CFunDeclr (Left lIdent) lCAttr nodeInfo)          = do
-	mapM_ (putStrLn . identToString) lIdent
-	mapM_ kk lCAttr
-
-ll :: CDecl -> IO()
-ll (CDecl lCDeclSpec l ni) = do
-	mapM_ i lCDeclSpec
-	mm l
-
-mm :: [(Maybe CDeclr, Maybe CInit, Maybe CExpr)] -> IO()
-mm l = mapM_ nn l
-
-nn :: (Maybe CDeclr, Maybe CInit, Maybe CExpr) -> IO()
-nn (a,b,c) = do
-	oo a
-	pp b
-	qq c
-
-oo :: Maybe CDeclr -> IO()
-oo Nothing = return ()
-oo (Just c) = ii c
-
-pp :: Maybe CInit -> IO()
-pp Nothing = return ()
-pp (Just c) = aaa c
-
-qq :: Maybe CExpr -> IO()
-qq Nothing = return ()
-qq (Just e) = expr e
-
-aaa :: CInit -> IO()
-aaa (CInitExpr cExpr _)     = putStrLn "cexpr"
-aaa (CInitList cInitList _) = putStrLn "cinitList"
-
-expr :: CExpr -> IO()
-expr (CComma lCExpr _) = mapM_ expr lCExpr
-expr (CConst cConst)   = constt cConst
-
-constt :: CConst -> IO()
-constt (CIntConst cInteger _)     = putStrLn $  show $ getCInteger cInteger
-constt (CCharConst cChar _)       = putStrLn $ getCChar cChar
-constt (CFloatConst (CFloat f) _) = putStrLn $ show f
-constt (CStrConst cString _)      = putStrLn $ getCString cString
-
-i :: CDeclSpec -> IO()
-i (CTypeSpec ctype) = typeSpec ctype
-
-typeSpec :: CTypeSpec -> IO()
-typeSpec (CVoidType _) = putStr "void "
-typeSpec (CIntType _)  = putStr "int "
 
 main :: IO ()
 main = do
--- Parser/main.c
+++ Parser/main.c
@@ -1,13 +1,23 @@
-
 void main(int argc) {
-	if(1) {
-	} else {
-	}
-	if(1) {
-	} else {
-	}
-	if(1) {
-	} else {
+	if(1) {}
+	if(1) {}
+	if(1) {}
+	if(1) {}
+	if(1) {}
+	if(1) {}
+	if(1) {}
+	if(1) {}
+	if(1) {}
+	if(1) {}
+	switch (1) {
+		case 1: {
+					return 1;
+					break;
+				}
+		case 1: {
+					return 1;
+					break;
+				}
 	}
 	return 0;
 }
