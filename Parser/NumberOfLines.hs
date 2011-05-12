19c3f721223d3bad2d674b2302b2938cf2d0f0b7
Ulisses Costa <ulissesmonhecosta@gmail.com>
2011-05-12 23:13:41 +0100

    implemented the module NumberOfLines.hs that will have number of lines related metrics. For now we already have the number of physical lines.

---
+++ Parser/NumberOfLines.hs
@@ -0,0 +1,31 @@
+------------------------------------------------------------------------------ 
+-- | 
+-- Author       : Ulisses Araujo Costa
+-- Stability    : experimental
+-- Portability  : portable
+--
+-- This module is part of 'Static-Code-Analyzer', a library to automatic
+-- calculate some metrics related with C99 and GNU C extensions code.
+--
+-- This module have some metrics related with number of lines of code and so
+--
+------------------------------------------------------------------------------
+
+module NumberOfLines(physicalLines) where
+
+import Language.C
+import Language.C.System.GCC
+import Language.C.Data.Ident
+import Language.C.Pretty
+
+{- Get the number of physical lines, y this we mean:
+   the number of lines of code, without blank lines
+   and without comments.
+-}
+physicalLines :: FilePath -> IO Int
+physicalLines file = do
+    parse <- parseCFile (newGCC "gcc") Nothing ["-U__BLOCKS__"] file
+    case parse of
+        (Left err)   -> return 0
+        (Right tree) -> (return . length . filter (not . null) . lines . show . pretty) tree
+
