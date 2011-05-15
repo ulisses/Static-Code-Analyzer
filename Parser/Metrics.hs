345cd169eecdbc874d7c5a4bca26d2f2b4106133
Ulisses Costa <ulissesmonhecosta@gmail.com>
2011-05-15 01:08:58 +0100

    implemented a pretty printing for our XML file, stupid HXT does not have it!

--- Parser/Metrics.hs
+++ Parser/Metrics.hs
@@ -18,12 +18,18 @@ import Text.XML.HXT.DOM.XmlNode
 import Text.XML.HXT.DOM.MimeTypes
 import Text.XML.HXT.DOM.ShowXml
 import Text.XML.HXT.DOM.TypeDefs
+import Text.XML.HXT.DOM.FormatXmlTree
 import Text.XML.HXT.Parser.XmlParsec
 import Data.Map
 
 data Metrics = Metrics (Map MetricName MetricValue)
+    deriving Show
 type MetricName = String
 type MetricValue = Double
+
+insertMetric :: (MetricName,MetricValue) -> Metrics -> Metrics
+insertMetric (mn,mv) r@(Metrics m) | member mn m = r
+                                   | otherwise   = Metrics $ insert mn mv m
 
 {- This function converts a metrics bag to XML
 -}
@@ -35,5 +41,22 @@ convertToXML (Metrics m) = [mkTree (XTag (mkName "metricx") [])  $ convertToXML_
           mkValue v       = (mkAttr (mkName "value")) [(mkTree (mkText v) [])]
           mkNameMetric k  = (mkAttr (mkName "name")) [(mkTree (mkText k) [])]
 
+{- Show Metrics in XML
+-}
+showXMLMetrics = putStrLn . indentXMLMetrics . convertToXML
+
+{- Indent XML metrics (HXL seems to not has pretty printing)
+-}
+indentXMLMetrics :: XmlTrees -> String
+indentXMLMetrics [ NTree (XTag tag _) l ]
+    =    "<" ++ localPart tag ++ ">"
+      ++  foldr (\h t -> "\n\t" ++ xshow [h] ++ t) "\n" l
+      ++ "</" ++ localPart tag ++ ">"
+
+{- Write metrics bag to file
+-}
+writeMetricsToFile :: FilePath -> Metrics -> IO ()
+writeMetricsToFile fn = writeFile fn . indentXMLMetrics . convertToXML
+
 test :: Metrics
-test = Metrics ( fromList([("John",34.0), ("Bob",12.1)]) )
+test = Metrics ( fromList([("Jo2hn",34.0),("Jo3hn",34.0),("Jo134hn",34.0),("J314ohn",34.0),("Joh43343n",34.0), ("Bob",12.1)]) )
