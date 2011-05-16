6297b54151310f08315cbea5b79b6bfae446421a
Ulisses Costa <ulissesmonhecosta@gmail.com>
2011-05-16 15:42:09 +0100

    description

--- Parser/Includes.hs
+++ Parser/Includes.hs
@@ -7,7 +7,7 @@
 -- This module is part of 'Static-Code-Analyzer', a library to automatic
 -- calculate some metrics related with C99 and GNU C extensions code.
 --
--- In this module I implement the metrics related with #includes pragmas
+-- In this module I implement the metrics related with #include pragma
 --
 ------------------------------------------------------------------------------
 
