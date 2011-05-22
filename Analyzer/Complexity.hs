51a53755f5a241c1ea3725651f0e43cadb0d79f5
Ulisses Costa <ulissesmonhecosta@gmail.com>
2011-05-22 01:09:43 +0100

    Modified Parser dir to Analyzer and added complexity module

---
+++ Analyzer/Complexity.hs
@@ -0,0 +1,14 @@
+------------------------------------------------------------------------------ 
+-- | 
+-- Author       : Ulisses Araujo Costa
+-- Stability    : experimental
+-- Portability  : portable
+--
+-- This module is part of 'Static-Code-Analyzer', a library to automatic
+-- calculate some metrics related with C99 and GNU C extensions code.
+--
+-- In this module I implement the metrics related with complexity
+--
+------------------------------------------------------------------------------
+module Complexity where
+
