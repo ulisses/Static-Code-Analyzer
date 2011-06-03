3c4a44f7fe664a043bc1f59dec0b44a2243b6658
Ulisses Costa <ulissesmonhecosta@gmail.com>
2011-06-03 20:37:11 +0100

    added function to calculate the absolute path of a file/folder

---
+++ Analyzer/AbsolutePath.hs
@@ -0,0 +1,26 @@
+------------------------------------------------------------------------------ 
+-- | 
+-- Author       : Ulisses Araujo Costa
+-- Stability    : experimental
+-- Portability  : portable
+--
+-- This module is part of 'Static-Code-Analyzer', a library to automatic
+-- calculate some metrics related with C99 and GNU C extensions code.
+--
+-- This is an auxiliar module to get the absolute path of a file/folder
+--
+------------------------------------------------------------------------------
+module AbsolutePath where
+
+import System.Directory
+import System.Path.NameManip
+
+mkAbsolutePath :: FilePath -> IO FilePath
+mkAbsolutePath fp = do
+    d <- getCurrentDirectory
+    let lstDot = takeWhile (=="..") $ slice_path $ normalise_path fp
+    let n = length lstDot
+    let rest = unslice_path $ dropWhile (=="..") $ slice_path $ normalise_path fp
+    return $ if n==0 then fp else normalise_path $ unsplit_path (remove n d,rest)
+    where remove 0 s = s
+          remove n s = remove (n-1) $ dir_part s
