5cdbdaa880d942b07772e9ce48e84cea16e1ef3b
Ulisses Costa <ulissesmonhecosta@gmail.com>
2011-06-25 18:04:07 +0100

    fixed a bug

--- Analyzer/Pdf.hs
+++ Analyzer/Pdf.hs
@@ -8,7 +8,7 @@ import Latex
 generatePDF out m = do
     let outT = toTex out
     generateLatex outT m
-    h <- runCommand ("latexmk -e '$pdflatex=q/pdflatex --shell-escape -synctex=1 %T/' -pdf "++ outT ++" > /dev/null 2> > /dev/null")
+    h <- runCommand ("latexmk -e '$pdflatex=q/pdflatex --shell-escape -synctex=1 %T/' -pdf "++ outT ++" > /dev/null 2> /dev/null")
     ex <- waitForProcess h
     return()
 
